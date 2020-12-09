32277bb6325f333ff50a57819950dec6120e2e1d
Swaraj Gore <43721080+swarajgore99@users.noreply.github.com>
2020-12-09 19:28:55 +0530

    Append Node

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class LinkedList{
+public:
+    Node *root = NULL;
+    void appendNode(int data){
+        Node *node = new Node(data);
+        if(root == NULL){
+            root = node;
+        }
+        else{
+            Node *temp = root;
+            while(temp->next != NULL)
+                temp = temp->next;
+            temp->next = node;
+        }
+    }
+};
+
+void printList(LinkedList list){
+    Node *node = list.root;
+    while(node != NULL){
+        cout<<node->data<<" ";
+        node = node->next;
+    }
+}
+int main() {
+    LinkedList list;
+    list.appendNode(1);
+    list.appendNode(2);
+    list.appendNode(3);
+    list.appendNode(4);
+    list.appendNode(5);
+    list.appendNode(6);
+    printList(list);
+    return 0;
+}
